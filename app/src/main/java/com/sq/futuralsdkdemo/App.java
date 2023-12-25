56700c37b6e612747114592e020f0a76e2281aaf
zhuangzi200323 <zhuangzi200323@gmail.com>
2023-12-25 15:31:33 +0800

    Add application permissions, App subclasses, network configuration and import service from sdk

---
+++ app/src/main/java/com/sq/futuralsdkdemo/App.java
@@ -0,0 +1,10 @@
+package com.sq.futuralsdkdemo;
+
+import com.sq.futuralsdk.FuturalApp;
+
+public class App extends FuturalApp {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
